15df67ddd24570b4aa3174baa00fc7318afb5269
Freezepeach <s3572290@student.rmit.edu.au>
2016-11-04 11:22:52 +1100

    Update calc_constants.h

--- calc_constants.h
+++ calc_constants.h
@@ -1,1 +1,7 @@
 /*This header file contains all constants used in the GPA-Calculator app.*/
+constexpr double unit_val = 12.0;
+constexpr double HD_grade = unit_val * 4.0;
+constexpr double D_grade = unit_val * 3.0;
+constexpr double C_grade = unit_val * 2.0;
+constexpr double P_grade = unit_val;
+constexpr double N_grade = 0.0;
